b7c88445ff6aa8c53375e13d5ae976d005c147c9
MAGICTNT <maxjavaux@gmail.com>
2024-08-22 10:20:41 +0200

    init projet est structure

---
+++ src/main/java/fr/maxime/entity/ConnectionManager.java
@@ -0,0 +1,38 @@
+package fr.maxime.entity;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static Connection connection = null;
+
+    private ConnectionManager() {
+        try {
+            String url = "jdbc:postgresql://localhost:5432/jdbc?currentSchema=exo1";
+            String username = "postgres";
+            String password = "2203";
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur lors de l'établissement de la connexion à la base de données", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+           new ConnectionManager();
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                throw new RuntimeException("Erreur lors de la fermeture de la connexion à la base de données", e);
+            }
+        }
+    }
+}
